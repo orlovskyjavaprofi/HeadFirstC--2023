c7ce36f8b55574f2b598a2990d207f0f129fab79
Alexander Orlovsky <javaprofi@gmx.de>
2023-02-19 16:00:11 +0100

    Add simple test to a WPF project
    Add simple test to a WPF project

---
+++ ExampleWPFGame/MatchGame/Tests/GameInitTests.cs
@@ -0,0 +1,25 @@
+﻿
+using MatchGame.GameCode;
+using NUnit.Framework;
+
+namespace MatchGame.Tests
+{
+    public class GameInitTests
+    {
+
+        private GameInitializer gameInitObj;
+
+        [SetUp]
+        public void setup()
+        {
+            gameInitObj = new GameInitializer();
+        }
+
+        [Test]
+        public void creationOfProgramObjTest()
+        {
+            Assert.NotNull(gameInitObj);
+        }
+
+    }
+}
