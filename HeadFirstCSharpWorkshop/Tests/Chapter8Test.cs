2e9026c7ebb2e42d0f028a8666ea7e0ee5e580e9
Alexander Orlovsky <javaprofi@gmx.de>
2023-03-19 13:25:36 +0100

    Base for the example of Generics , Interface, Collections
    Making ground for the example of interface, collection and Generics.

---
+++ HeadFirstCSharpWorkshop/Tests/Chapter8Test.cs
@@ -0,0 +1,28 @@
+﻿using HeadFirstCSharpWorkshop.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharpWorkshop.Tests
+{
+    public class Chapter8Test
+    {
+        private Chapter8 chapter8Obj;
+
+        [SetUp]
+        public void setup()
+        {
+            chapter8Obj = new Chapter8();
+        }
+
+
+        [Test]
+        public void creationOfChapter8Test()
+        {
+            Assert.NotNull(chapter8Obj);
+        }
+    }
+}
+
