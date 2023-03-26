bfacdbef791ca0236cb6a7f72a88f80776d4bf22
Alexander Orlovsky <javaprofi@gmx.de>
2023-03-26 15:32:51 +0200

    Added bases clases for thread example
    Added bases clases for thread example

---
+++ HeadFirstCSharpWorkshop/Tests/TrainStationTest.cs
@@ -0,0 +1,26 @@
+﻿using HeadFirstCSharpWorkshop.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharpWorkshop.Tests
+{
+    public class TrainStationTest
+    {
+        private TrainStation trainStationObj;
+
+        [SetUp]
+        public void setup()
+        {
+            trainStationObj = new TrainStation();
+        }
+
+        [Test]
+        public void creationOfTrainStationTest()
+        {
+            Assert.NotNull(trainStationObj);
+        }
+    }
+}
---
+++ HeadFirstCSharpWorkshop/Tests/TrainTest.cs
@@ -0,0 +1,26 @@
+﻿using HeadFirstCSharpWorkshop.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharpWorkshop.Tests
+{
+    public class TrainTest
+    {
+        private Train trainObj;
+
+        [SetUp]
+        public void setup()
+        {
+            trainObj = new Train();
+        }
+
+        [Test]
+        public void creationOfTrainTest()
+        {
+            Assert.NotNull(trainObj);
+        }
+    }
+}
